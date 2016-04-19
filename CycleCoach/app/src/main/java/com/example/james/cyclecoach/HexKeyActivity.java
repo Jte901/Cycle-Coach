315551145bc1eb13f5255b8dc6451156d2307ae8
James Elliott <jte901@vt.edu>
2016-04-18 23:10:49 -0400

    More work on hex key

--- CycleCoach/app/src/main/java/com/example/james/cyclecoach/HexKeyActivity.java
+++ CycleCoach/app/src/main/java/com/example/james/cyclecoach/HexKeyActivity.java
@@ -11,14 +11,16 @@ import android.text.format.DateFormat;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TimePicker;
 
 import java.util.Calendar;
 
-public class HexKeyActivity extends AppCompatActivity {
+public class HexKeyActivity extends AppCompatActivity implements View.OnClickListener {
 
-    Button mTimeButton;
+    Button mTimeButton, mSetItButton;
     Spinner mReccurenceSpinner, mTimeSpinner;
 
     @Override
@@ -27,15 +29,30 @@ public class HexKeyActivity extends AppCompatActivity {
         setContentView(R.layout.activity_hex_key);
 
         mTimeButton = (Button) findViewById(R.id.timeButton);
+        mSetItButton = (Button) findViewById(R.id.setItButton);
+        mSetItButton.setOnClickListener(this);
+
+        RadioGroup difficulty = (RadioGroup) findViewById(R.id.radioGroupDifficulty);
+        RadioGroup smartWatch = (RadioGroup) findViewById(R.id.radioGroupSmartwatch);
+        RadioGroup nfc = (RadioGroup) findViewById(R.id.radioGroupNfc);
+
+        RadioButton mediumButton = (RadioButton) findViewById(R.id.mediumRadioButton);
+        RadioButton enabledWatchButton = (RadioButton) findViewById(R.id.smartWatchEnabledRadioButton);
+        RadioButton enabledNfcButton = (RadioButton) findViewById(R.id.nfcEnabledRadioButton);
+
+        difficulty.check(mediumButton.getId());
+        smartWatch.check(enabledWatchButton.getId());
+        nfc.check(enabledNfcButton.getId());
+
         mReccurenceSpinner = (Spinner) findViewById(R.id.recurrenceSpinner);
         mTimeSpinner = (Spinner) findViewById(R.id.timesSpinner);
 
         ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(this,
-                R.array.notificationTimes, android.R.layout.simple_spinner_item);
+                R.array.notificationTimes, R.layout.spinner_layout);
         adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
         ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this,
-                R.array.delayTimes, android.R.layout.simple_spinner_item);
+                R.array.delayTimes, R.layout.spinner_layout);
         adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
         mReccurenceSpinner.setAdapter(adapter1);
@@ -46,6 +63,16 @@ public class HexKeyActivity extends AppCompatActivity {
     public void showTimePickerDialog(View v) {
         DialogFragment newFragment = new TimePickerFragment();
         newFragment.show(getFragmentManager(), "timePicker");
+    }
+
+    /**
+     * Called when a view has been clicked.
+     *
+     * @param v The view that was clicked.
+     */
+    @Override
+    public void onClick(View v) {
+        finish();
     }
 
     public static class TimePickerFragment extends DialogFragment
