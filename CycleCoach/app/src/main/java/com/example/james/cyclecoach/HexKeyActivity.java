1ffebf20fdeb3c6b0709945fb20098cbe80be0da
James Elliott <jte901@vt.edu>
2016-04-25 16:37:59 -0400

    more work on hex key

--- CycleCoach/app/src/main/java/com/example/james/cyclecoach/HexKeyActivity.java
+++ CycleCoach/app/src/main/java/com/example/james/cyclecoach/HexKeyActivity.java
@@ -5,10 +5,13 @@ import android.app.Dialog;
 import android.app.DialogFragment;
 import android.app.FragmentManager;
 import android.app.TimePickerDialog;
+import android.graphics.Color;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.format.DateFormat;
 import android.view.View;
+import android.view.Window;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.RadioButton;
@@ -16,17 +19,38 @@ import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TimePicker;
 
+import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.StringReader;
+import java.io.StringWriter;
 import java.util.Calendar;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 
 public class HexKeyActivity extends AppCompatActivity implements View.OnClickListener {
 
     Button mTimeButton, mSetItButton;
     Spinner mReccurenceSpinner, mTimeSpinner;
+    Document doc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hex_key);
+
+        Window window = this.getWindow();
+        window.setStatusBarColor(Color.parseColor("#000000"));
+
+        initFile();
 
         mTimeButton = (Button) findViewById(R.id.timeButton);
         mSetItButton = (Button) findViewById(R.id.setItButton);
@@ -37,8 +61,19 @@ public class HexKeyActivity extends AppCompatActivity implements View.OnClickLis
         RadioGroup nfc = (RadioGroup) findViewById(R.id.radioGroupNfc);
 
         RadioButton mediumButton = (RadioButton) findViewById(R.id.mediumRadioButton);
+        mediumButton.setOnClickListener(this);
+        RadioButton hardButton = (RadioButton) findViewById(R.id.hardRadioButton);
+        hardButton.setOnClickListener(this);
+
         RadioButton enabledWatchButton = (RadioButton) findViewById(R.id.smartWatchEnabledRadioButton);
+        enabledWatchButton.setOnClickListener(this);
+        RadioButton disabledWatchButton = (RadioButton) findViewById(R.id.smartWatchDisabledRadioButton);
+        disabledWatchButton.setOnClickListener(this);
+
         RadioButton enabledNfcButton = (RadioButton) findViewById(R.id.nfcEnabledRadioButton);
+        enabledNfcButton.setOnClickListener(this);
+        RadioButton disabledNfcButton = (RadioButton) findViewById(R.id.nfcDisabledRadioButton);
+        disabledNfcButton.setOnClickListener(this);
 
         difficulty.check(mediumButton.getId());
         smartWatch.check(enabledWatchButton.getId());
@@ -59,6 +94,27 @@ public class HexKeyActivity extends AppCompatActivity implements View.OnClickLis
         mTimeSpinner.setAdapter(adapter2);
     }
 
+    private void initFile() {
+        File nameFile = new File(Environment.getExternalStorageDirectory().getAbsolutePath(), "CycleCoach_name.xml");
+        if (nameFile.exists()) {
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            try {
+                FileInputStream fis = new FileInputStream(nameFile);
+                byte[] d = new byte[(int) nameFile.length()];
+                fis.read(d);
+                fis.close();
+                //  this.data.name = new String(d, "UTF-8");
+                DocumentBuilder db = dbf.newDocumentBuilder();
+                InputSource is = new InputSource();
+                is.setCharacterStream(new StringReader(new String(d, "UTF-8")));
+                doc = db.parse(is);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
 
     public void showTimePickerDialog(View v) {
         DialogFragment newFragment = new TimePickerFragment();
@@ -72,7 +128,36 @@ public class HexKeyActivity extends AppCompatActivity implements View.OnClickLis
      */
     @Override
     public void onClick(View v) {
-        finish();
+        if (v.getId() == R.id.mediumRadioButton) {
+            doc.getElementsByTagName("difficulty").item(0).setTextContent("medium");
+        } else if (v.getId() == R.id.hardRadioButton) {
+            doc.getElementsByTagName("difficulty").item(0).setTextContent("hard");
+        } else if (v.getId() == R.id.smartWatchEnabledRadioButton) {
+            doc.getElementsByTagName("smartwatch").item(0).setTextContent("enabled");
+        } else if (v.getId() == R.id.smartWatchDisabledRadioButton) {
+            doc.getElementsByTagName("smartwatch").item(0).setTextContent("disabled");
+        } else if (v.getId() == R.id.nfcEnabledRadioButton) {
+            doc.getElementsByTagName("nfc_enabled").item(0).setTextContent("enabled");
+        } else if (v.getId() == R.id.nfcDisabledRadioButton) {
+            doc.getElementsByTagName("nfc_enabled").item(0).setTextContent("enabled");
+        }
+        if (v.getId() == R.id.setItButton) {
+            try {
+                Transformer transformer = TransformerFactory.newInstance().newTransformer();
+                StringWriter writer = new StringWriter();
+                StreamResult result = new StreamResult(writer);
+                transformer.transform(new DOMSource(doc), result);
+                File externalDir = Environment.getExternalStorageDirectory();
+                File notes = new File(externalDir, "CycleCoach_name.xml");
+                FileOutputStream os = new FileOutputStream(notes);
+                os.write(writer.toString().getBytes());
+                os.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                finish();
+            }
+        }
     }
 
     public static class TimePickerFragment extends DialogFragment
@@ -93,11 +178,10 @@ public class HexKeyActivity extends AppCompatActivity implements View.OnClickLis
         public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
             // Do something with the time chosen by the user
             HexKeyActivity parentActivity = (HexKeyActivity) getActivity();
-            if (hourOfDay >=12 ) {
+            if (hourOfDay >= 12) {
                 if (hourOfDay >= 13) hourOfDay -= 12;
                 parentActivity.mTimeButton.setText(String.format("%d:%02d pm", hourOfDay, minute));
-            }
-            else {
+            } else {
                 if (hourOfDay == 0) hourOfDay = 12;
                 parentActivity.mTimeButton.setText(String.format("%d:%02d am", hourOfDay, minute));
             }
